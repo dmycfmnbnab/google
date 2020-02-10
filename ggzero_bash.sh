7c7cfe185975030fda42868decf2db6e7865fdaa
dmycfmnbnab <60755272+dmycfmnbnab@users.noreply.github.com>
2020-02-10 17:48:34 +0800

    Update ggzero_bash.sh

--- ggzero_bash.sh
+++ ggzero_bash.sh
@@ -18,10 +18,10 @@ cd /content
 rm -f ggzero_linux
 rm -f lc0
 
-wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
-wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/lc0
+wget -c https://github.com/semblack/google/raw/master/train
+wget -c https://github.com/semblack/google/raw/master/lc0
 chmod 777 lc0
-chmod 777 ggzero_linux
+chmod 777 train
 
 
 
