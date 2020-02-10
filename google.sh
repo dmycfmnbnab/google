8f253aedfb3c25f296aa35c14c0201368e4cf3a6
semblack <49827894+semblack@users.noreply.github.com>
2020-02-10 17:22:17 +0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -4,5 +4,3 @@ wget -c https://github.com/semblack/google/raw/master/train
 wget -c https://github.com/semblack/google/raw/master/lc0
 chmod 777 lc0
 chmod 777 train
-
-./train --user '海森堡' --password 'qq123456' >/dev/null
